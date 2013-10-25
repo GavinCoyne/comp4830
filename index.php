15c9c0f77ec63056067d2353bc42bd301c0b6ffd
Gavin Coyne <speed.sk8@gmail.com>
2013-10-25 14:37:02 -0700

    playing with function calls

--- index.php
+++ index.php
@@ -4,21 +4,29 @@ include __dir__.'/controller/template.php';
 	
 class Page extends template
 {
-	public function __construct() {
+	public function __construct($function) {
        parent::__construct();
+       var_dump($function);
+       
+       self::$function();
    }
-
 	
+   public function test()
+	{
+		echo "This is a test function"
+	}
 }
 
 
 	
 	
-	
+//Templating controller	
 	if(isset($_GET['action']))
 	{
 		echo '<link rel="stylesheet" type="text/css" href="css/index.css" />';
-	
-		$test = new page();
+		$func = (isset($_GET["func"]) ? $_GET["func"] : null);
+		
+		//i.e. http://localhost/comp4830/?action=index&func=test
+		$test = new page($func);
 		
 	}
