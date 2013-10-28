a636ba1317ae19d7faa6184fdb53b344c32c139b
Jonathan Halls <johalls@hotmail.com>
2013-10-28 13:23:44 -0700

    Added envVars.php for setup of environment variables

---
+++ model/envVars.php
@@ -0,0 +1,6 @@
+<?php 
+	$env = ".cmd"; //This is for a windows environment
+	//$env = ".sh"; //This is for a linux environment
+	
+	$JAVA_HOME = "\"C:\Program Files (x86)\Java\jdk1.7.0_40\""; //Set your java path here
+?>
