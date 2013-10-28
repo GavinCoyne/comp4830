a8c03fc7342d2303e93474cfc17bc99d976a42c3
Gavin Coyne <speed.sk8@gmail.com>
2013-10-27 22:29:57 -0700

    editor stuff

--- js/system.js
+++ js/system.js
@@ -1,13 +1,17 @@
 $(document).ready(function(){
 	hljs.initHighlighting();
 	
-	$("#editorInput").keydown(function(e){
+	$("#editorInput").keypress(function(e){
 		console.log('.change');
 		 var code = e.keyCode || e.which;
-		
+		 
 			$('#editorWindow').append($('#editorInput').val());
 			$('#editorInput').val('');
 			hljs.highlightAuto('#editorWindow').value;
+			if(code == 13) { //Enter keycode
+				 
+				$('#editorWindow').append('<br>');
+			 }
 		
 	});
 	
